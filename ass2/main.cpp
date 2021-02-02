398a431fae224a3d2b31a2d618f823a40deed49a
Asha jyothi Donga <58396795+asha24102001@users.noreply.github.com>
2021-02-02 19:04:19 +0530

    Update main.cpp

--- ass2/main.cpp
+++ ass2/main.cpp
@@ -34,7 +34,9 @@ int main()
            }
            k++; j--;
         }
-       
+        if(maxlen<len)
+              {maxlen=len; x=j+1; y=k-1; direction=0; }
+              
             j=i-1; k=1; len=1;
             while(j>=0 && k<=n-1)
             {
@@ -49,6 +51,9 @@ int main()
                  k++; j--;
              
            }
+        if(maxlen<len)
+              {maxlen=len; x=j+1; y=k-1; direction=0; }
+             
 
     }
     //digonals from bottom-right to top-left direction=1 for this type of digonals
@@ -67,7 +72,9 @@ int main()
             }
             j--; k--;
         }
-         
+         if(maxlen<len)
+              {maxlen=len; x=j+1; y=k-1; direction=1; }
+             
             j=i-1; k=n-2; len=1;
             while(j>=0 && k>=0)
            {
@@ -81,6 +88,9 @@ int main()
             }
             j--; k--;
            }
+        if(maxlen<len)
+              {maxlen=len; x=j+1; y=k-1; direction=1; }
+            
         
     }
     //printing the elements
